858561fa4153a5ef863f1443584f77077ce49695
Chris Roberts <code@chrisroberts.org>
2015-05-04 09:43:11 -0700

    Add service registration information

--- lib/jackal-commander.rb
+++ lib/jackal-commander.rb
@@ -6,3 +6,14 @@ module Jackal
     autoload :Executor, 'jackal-commander/executor'
   end
 end
+
+Jackal.service(
+  :commander,
+  :description => 'Run arbitrary commands',
+  :configuration => {
+    :actions => {
+      :description => 'Named commands payload will reference',
+      :type => :hash
+    }
+  }
+)
